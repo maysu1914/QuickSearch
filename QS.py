ba54c8a453cc8c374a2f7ccf36147c707cd4ff35
maysu1914 <mustafa.aysu@hotmail.com>
2020-11-07 22:35:19 +0300

    main class created, some improvements may need, test phase now.

--- QS.py
+++ QS.py
@@ -676,87 +676,114 @@ class VatanBilgisayar(SourceWebSite):
         return products
 
 
-def source_controller(category):
-    sources = [VatanBilgisayar, N11, HepsiBurada, Trendyol, AmazonTR, Teknosa, GittiGidiyor, MediaMarktTR]
-    source_selections = None
-    results = []
-    correct_results = []
-    near_results = []
-    supported_sources = []
+class QuickSearch:
+    max_page = 3
 
-    print("\nSelect the sources you want to search:")
-    index = 1
-    for source in sources:
-        if category in source.get_categories():
-            supported_sources.append(source)
-            print(str(index) + '.', source.source_name)
-            index += 1
-        else:
-            pass
+    def __init__(self, max_page=max_page):
+        self.sources = [VatanBilgisayar, N11, HepsiBurada, Trendyol, AmazonTR, Teknosa, GittiGidiyor, MediaMarktTR]
+        self.categories = ['All', 'Notebooks', 'Smartphones', 'Monitors']
+        self.max_page = max_page
+        self.category_selection = None
+        self.source_selections = []
+        self.search_text = None
+        self.raw_results = []
+        self.correct_results = []
+        self.near_results = []
 
-    while source_selections is None or any(
-            source_selection not in [str(num) for num in range(0, len(supported_sources) + 1)] for source_selection in
-            source_selections):
-        source_selections = [source_selection.strip() for source_selection in input('Sources: ').split(',')]
-        if '0' in source_selections:
-            source_selections = [str(num) for num in range(1, len(supported_sources) + 1)]
+    def search(self):
+        self.category_selection = self.get_category_input()
+        self.source_selections = self.get_source_input()
+        self.search_text = self.get_search_input()
+        self.get_results()
+        self.show_results()
 
-    search_input = input('\nSearch Text: ').strip()
+    def get_category_input(self):
+        category_selection = []
+        print("What category do you want to search?")
 
-    processes = []
-    # print(os.cpu_count())
-    with Pool() as pool:
-        for source in source_selections:
-            processes.append(
-                pool.apply_async(supported_sources[int(source) - 1](category, max_page=1).search, (search_input,)))
-        for process in processes:
-            results += process.get()
+        for index in range(1, len(self.categories)):
+            print(str(index) + '.', self.categories[index])
 
-    unique_results = []
-    seen = set()
-    for result in results:
-        t = tuple(result.items())
-        if t not in seen:
-            seen.add(t)
-            unique_results.append(result)
+        while category_selection not in [str(num) for num in range(0, len(self.categories))]:
+            category_selection = input('Category: ').strip()
 
-    for i in sorted(unique_results, key=lambda i: [-i['suitable_to_search'], int(i['price'].split()[0])]):
-        if i['suitable_to_search']:
-            correct_results.append(i)
-        else:
-            t = i.copy()
-            t['suitable_to_search'] = True
-            t = tuple(t.items())
-            if t not in seen:
-                near_results.append(i)
+        return self.categories[int(category_selection)]
+
+    def get_source_input(self):
+        source_selections = []
+        supported_sources = []
+        print("\nSelect the sources you want to search:")
+
+        index = 1
+        for source in self.sources:
+            if self.category_selection in source.get_categories():
+                supported_sources.append(source)
+                print(str(index) + '.', source.source_name)
+                index += 1
             else:
-                # print(i)
                 pass
 
-    print("\nResults:") if correct_results else ''
-    for product in correct_results:
-        print(product['source'], product['name'], product['price'], product['info'], product['comment_count'])
+        while not source_selections or any(
+                int(source_selection) not in range(len(supported_sources) + 1) for source_selection in
+                source_selections):
+            source_selections = [source_selection.strip() for source_selection in input('Sources: ').split(',')]
+            if '0' in source_selections:
+                source_selections = range(1, len(supported_sources) + 1)
 
-    print("\nYou may want to look at these:") if near_results else ''
-    for product in near_results:
-        print(product['source'], product['name'], product['price'], product['info'], product['comment_count'])
+        self.sources = supported_sources
 
-    print("_________________________________\n")
+        return source_selections
 
+    @staticmethod
+    def get_search_input():
+        search_input = input('\nSearch Text: ').strip()
+        return search_input
 
-def main():
-    categories = ['All', 'Notebooks', 'Smartphones', 'Monitors']
-    category_selection = None
+    def get_results(self):
+        processes = []
+        # print(os.cpu_count())
+        with Pool() as pool:
+            for source in self.source_selections:
+                processes.append(
+                    pool.apply_async(self.sources[int(source) - 1](self.category_selection, max_page=self.max_page).search,
+                                     (self.search_text,)))
+            for process in processes:
+                self.raw_results += process.get()
 
-    print("What category do you want to search?")
-    for index in range(1, len(categories)):
-        print(str(index) + '.', categories[index])
-    while category_selection not in [str(num) for num in range(0, len(categories))]:
-        category_selection = input('Category: ').strip()
+        unique_results = []
+        seen = set()
+        for result in self.raw_results:
+            t = tuple(result.items())
+            if t not in seen:
+                seen.add(t)
+                unique_results.append(result)
 
-    source_controller(categories[int(category_selection)])
+        for i in sorted(unique_results, key=lambda i: [-i['suitable_to_search'], int(i['price'].split()[0])]):
+            if i['suitable_to_search']:
+                self.correct_results.append(i)
+            else:
+                t = i.copy()
+                t['suitable_to_search'] = True
+                t = tuple(t.items())
+                if t not in seen:
+                    self.near_results.append(i)
+                else:
+                    # print(i)
+                    pass
+
+    def show_results(self):
+        print("\nResults:") if self.correct_results else ''
+        for product in self.correct_results:
+            print(product['source'], product['name'], product['price'], product['info'], product['comment_count'])
+
+        print("\nYou may want to look at these:") if self.near_results else ''
+        for product in self.near_results:
+            print(product['source'], product['name'], product['price'], product['info'], product['comment_count'])
+
+        print("_________________________________\n")
 
 
 if __name__ == "__main__":
     while True:
-        main()
+        qs = QuickSearch(max_page=1)
+        qs.search()
