c4764d349e9bbdfaaf98a63ffce0bf0af354339c
maysu1914 <aysum1914@gmail.com>
2021-03-13 03:02:13 +0300

    Improve readability; Optimize main algorithms

--- QuickSearch-runner.py
+++ QuickSearch-runner.py
@@ -1,6 +1,12 @@
 from QuickSearch.QuickSearch import QuickSearch
 
 if __name__ == '__main__':
+    print("QuickSearch v0.9.41")
+    print("Copyright (c) 2020 maysu1914.")
+    print("")
+    print("Check new versions of QuickSearch from https://github.com/maysu1914/QuickSearch")
+    print("")
+
     while True:
         qs = QuickSearch(max_page=1)
         qs.search()
--- QuickSearch/QuickSearch.py
+++ QuickSearch/QuickSearch.py
@@ -1,4 +1,5 @@
 from concurrent.futures.thread import ThreadPoolExecutor
+from concurrent import futures
 
 from .websites.AmazonTR import AmazonTR
 from .websites.FLO import FLO
@@ -15,10 +16,13 @@ class QuickSearch:
     max_page = 3
 
     def __init__(self, max_page=max_page):
-        self.sources = [VatanBilgisayar, N11, HepsiBurada, Trendyol, AmazonTR, Teknosa, GittiGidiyor, MediaMarktTR, FLO]
+        self.sources = (VatanBilgisayar, N11, HepsiBurada, Trendyol, AmazonTR, Teknosa, GittiGidiyor, MediaMarktTR, FLO)
         self.categories = self.get_categories()
         self.max_page = max_page
+        self.executor = ThreadPoolExecutor()
+
         self.category_selection = None
+        self.sources_of_category = []
         self.source_selections = []
         self.search_text = None
         self.raw_results = []
@@ -31,8 +35,6 @@ class QuickSearch:
             for category in source.get_categories():
                 if category not in categories:
                     categories.append(category)
-                else:
-                    pass
         return categories
 
     def search(self):
@@ -46,39 +48,42 @@ class QuickSearch:
         category_selection = []
         print("What category do you want to search?")
 
-        for index in range(1, len(self.categories)):
-            print(str(index) + '.', self.categories[index])
+        for index, category in enumerate(self.categories):
+            print(str(index) + '.', category)
 
-        while category_selection not in [str(num) for num in range(0, len(self.categories))]:
-            category_selection = input('Category: ').strip()
+        while category_selection not in list(range(0, len(self.categories))):
+            try:
+                category_selection = int(input('Category: ').strip())
+            except ValueError:
+                category_selection = None
 
-        return self.categories[int(category_selection)]
+        return self.categories[category_selection]
 
     def get_source_input(self):
         source_selections = []
-        supported_sources = []
         print("\nSelect the sources you want to search:")
 
-        index = 1
         for source in self.sources:
             if self.category_selection in source.get_categories():
-                supported_sources.append(source)
-                print(str(index) + '.', source.source_name)
-                index += 1
-            else:
-                pass
+                self.sources_of_category.append(source)
 
-        while not source_selections or \
-                any(
-                    not source_selection.isnumeric() or int(source_selection) not in range(len(supported_sources) + 1)
-                    for source_selection in source_selections
-                ):
-            source_selections = [source_selection.strip() for source_selection in
-                                 input('Sources: ').split(',')]
-            if '0' in source_selections:
-                source_selections = [str(i) for i in range(1, len(supported_sources) + 1)]
+        print(str(0) + '.', 'All')
+        for index, source in enumerate(self.sources_of_category, start=1):
+            print(str(index) + '.', source.source_name)
 
-        self.sources = supported_sources
+        while not source_selections:
+            try:
+                source_selections = [int(source_selection) for source_selection in input('Sources: ').split(',')]
+                # if All option is selected
+                if 0 in source_selections:
+                    source_selections = list(range(len(self.sources_of_category)))
+                else:
+                    # if not all, store the index rather than storing selection
+                    source_selections = [source_selection - 1 for source_selection in source_selections]
+                    # to trigger possible IndexError exception
+                    [self.sources_of_category[source_selection] for source_selection in source_selections]
+            except (ValueError, IndexError):
+                source_selections = []
 
         return source_selections
 
@@ -90,34 +95,31 @@ class QuickSearch:
     def get_results(self):
         threads = []
 
-        for source in self.source_selections:
-            threads.append(ThreadPoolExecutor().submit(
-                self.sources[int(source) - 1](self.category_selection, max_page=self.max_page).search,
-                self.search_text))
+        for source_selection in self.source_selections:
+            arguments = [self.category_selection]
+            keyword_arguments = {"max_page": self.max_page}
+            source_object = self.sources_of_category[source_selection]
+            thread = self.executor.submit(source_object(*arguments, **keyword_arguments).search, self.search_text)
+            threads.append(thread)
 
-        for thread in threads:
+        for thread in futures.as_completed(threads):
             self.raw_results += thread.result()
 
         unique_results = []
-        seen = set()
+        seen = set()  # to skip same products from different results
         for result in self.raw_results:
-            t = tuple(result.items())
-            if t not in seen:
-                seen.add(t)
+            r = (result['source'], result['name'], result['price'], result['info'])
+            # check above data only for duplicate check
+            if r not in seen:
+                seen.add(r)
                 unique_results.append(result)
 
-        for i in sorted(unique_results, key=lambda i: [-i['suitable_to_search'], int(i['price'].split()[0])]):
-            if i['suitable_to_search']:
-                self.correct_results.append(i)
+        # sort results by price and split two
+        for result in sorted(unique_results, key=lambda i: [int(i['price'].split()[0])]):
+            if result['suitable_to_search']:
+                self.correct_results.append(result)
             else:
-                t = i.copy()
-                t['suitable_to_search'] = True
-                t = tuple(t.items())
-                if t not in seen:
-                    self.near_results.append(i)
-                else:
-                    # print(i)
-                    pass
+                self.near_results.append(result)
 
     def show_results(self):
         print("\nResults:") if self.correct_results else ''
