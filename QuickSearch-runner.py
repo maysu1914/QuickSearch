50d834213e662ce0a885879d2fa6493653c6f288
maysu1914 <aysum1914@gmail.com>
2021-04-27 22:44:12 +0300

    Add updated executable file

--- QuickSearch-runner.py
+++ QuickSearch-runner.py
@@ -1,12 +1,12 @@
 from QuickSearch.QuickSearch import QuickSearch
 
 if __name__ == '__main__':
-    print("QuickSearch v0.9.48")
+    print("QuickSearch v0.9.49")
     print("Copyright (c) 2020 maysu1914.")
     print("")
     print("Check new versions of QuickSearch from https://github.com/maysu1914/QuickSearch")
     print("")
 
     while True:
-        qs = QuickSearch(max_page=1)
+        qs = QuickSearch(max_page=3)
         qs.search()
