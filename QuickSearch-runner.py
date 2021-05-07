43107ba2e53b82ae1ff21399d3d7f058861681ed
maysu1914 <mustafa.aysu@hotmail.com>
2021-05-07 19:31:27 +0300

    Update version number

--- QuickSearch-runner.py
+++ QuickSearch-runner.py
@@ -1,7 +1,7 @@
 from QuickSearch.QuickSearch import QuickSearch
 
 if __name__ == '__main__':
-    print("QuickSearch v0.9.7.1")
+    print("QuickSearch v0.9.7.2")
     print("Copyright (c) 2021 maysu1914.")
     print("")
     print("Check new versions of QuickSearch from https://github.com/maysu1914/QuickSearch")
