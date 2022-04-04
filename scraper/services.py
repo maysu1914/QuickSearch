6adc5c22387a7c72200c150e2ce1211fdb1dfa53
maysu1914 <mustafa.aysu@hotmail.com>
2022-04-05 00:35:52 +0300

    remove verify False if request gave ssl error, don't pass ssl verification add session method for getting responses some code refactoring

--- scraper/services.py
+++ scraper/services.py
@@ -2,58 +2,61 @@ import concurrent
 import math
 import time
 from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
 from ssl import SSLError
 
 import requests
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 class RequestService:
 
-    def __init__(self, sleep=None):
+    def __init__(self, sleep=0):
         self.sleep = sleep
         self.executor = ThreadPoolExecutor()
 
     @staticmethod
-    def _get_page_content(url, counter=3, dynamic_verification=False):
+    @lru_cache
+    def _get_headers():
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0)',
+            "Accept": "text/html,application/xhtml+xml,application/xml,application/signed-exchange"
+        }
+        return headers
+
+    @staticmethod
+    @lru_cache
+    def _get_session():
+        session = requests.Session()
+        retries = Retry(total=3, backoff_factor=0.5)
+        session.mount('https://', HTTPAdapter(max_retries=retries))
+        return session
+
+    def _request(self, url, **kwargs):
+        kwargs.update({"headers": self._get_headers()})
+        session = self._get_session()
+        return session.get(url, **kwargs)
+
+    def _get_page_content(self, url, **kwargs):
         """
         Content retriever
-        :param sleep: time to sleep after request to prevent blocking of some sources
-        :param dynamic_verification: try without SSL verify if needed
         :param url: the link whose content is to be returned
-        :param counter: how many times of retrying
         :return: content of response
         """
-        error_count = 0
         print(url)
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36',
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
-        }
-        kwargs = {"timeout": 10, "headers": headers}
-        for count in range(1, counter + 1):
-            try:
-                return requests.get(url, **kwargs).content
-            except SSLError as e:
-                error_count += 1
-                print('Error occurred while getting page content!', count, url, e)
-                if error_count >= 3:
-                    raise requests.exceptions.RequestException(e)
-                if dynamic_verification:
-                    kwargs.update({"verify": False})
-            except Exception as e:
-                error_count += 1
-                print('Error occurred while getting page content!', count, url, e)
-                if error_count >= 3:
-                    raise requests.exceptions.RequestException(e)
-        return ""
+        try:
+            return self._request(url, **kwargs).content
+        except requests.exceptions.RequestException as e:
+            print('Error occurred while getting page content!', url, e)
+            raise
 
     def get_page_contents(self, url_list):
         error_count = 0
         threads = []
-        for index, url in enumerate(url_list):
-            threads.append(self.executor.submit(self._get_page_content, url, dynamic_verification=True))
-            if self.sleep:
-                time.sleep(self.sleep)
+        for url in url_list:
+            threads.append(self.executor.submit(self._get_page_content, url))
+            time.sleep(self.sleep)
         for thread in concurrent.futures.as_completed(threads):
             try:
                 yield thread.result()
@@ -61,4 +64,4 @@ class RequestService:
                 error_count += 1
                 print('Too much error occurred while getting the page contents!', e, url_list)
                 if error_count >= math.ceil(len(url_list) / 3):
-                    raise requests.exceptions.RequestException(e)
+                    raise
