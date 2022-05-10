62ea16411ad90433bf71d45ee3e8f06c98c4e46f
maysu1914 <mustafa.aysu@hotmail.com>
2022-05-10 22:23:43 +0300

    fix 0 is None issue by None type checking

--- scraper/utils.py
+++ scraper/utils.py
@@ -2,7 +2,7 @@ def get_attribute_by_path(dictionary, attribute_path, default=None):
     current_attr = dictionary
     for key in list(filter(lambda x: x, attribute_path.split('.'))):
         current_attr = current_attr.get(key)
-        if not current_attr:
+        if current_attr is None:
             return default
     else:
         return current_attr
