fb61ce08bbaaebd815e0322e03c3d84f4262bfc0
maysu1914 <mustafa.aysu@hotmail.com>
2023-05-01 13:01:43 +0300

    rename some files

--- quick_search/main.py
+++ quick_search/main.py
@@ -1,4 +1,3 @@
-import re
 from ast import literal_eval
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
@@ -8,73 +7,13 @@ from urllib.parse import urlparse
 from colorit import init_colorit, background, color
 
 from cli_prompts import Prompt
-from scraper.models import Scraper
+from quick_search.prompts import PromptSource, PromptURL
+from scraper.service import Scraper
 from scraper.utils import get_attribute_by_path, log_time
 
 EXECUTOR = ThreadPoolExecutor(max_workers=8)
 
 init_colorit()
-
-
-class PromptURL(Prompt):
-
-    def is_valid(self):
-        # https://regexr.com/39nr7
-        regex = r'[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)'
-        if self._data and re.match(regex, self._data):
-            hostname = urlparse(self._data).hostname
-            if self.choices:
-                if hostname in self.choices.values():
-                    self._valid = True
-                else:
-                    self._valid = False
-            else:
-                self._valid = True
-        else:
-            self._valid = False
-        return self._valid
-
-    def render(self):
-        title = self._get_title()
-        if title:
-            print(title)
-
-
-class PromptSource(Prompt):
-
-    def _normalize_data(self):
-        try:
-            source_selections = {s for s in self._data.split(',')}
-            if '0' in source_selections:
-                source_selections.update(self.choices.keys())
-                source_selections.discard('0')
-            source_selections = {int(i) for i in source_selections}
-            negatives = set([i for i in source_selections if i < 0])
-            discarded_positives = set([abs(i) for i in negatives])
-            source_selections -= negatives | discarded_positives
-            source_selections = {str(i) for i in source_selections}
-            self._data = source_selections
-        except ValueError:
-            pass
-
-    def is_valid(self):
-        data = self._data
-        if self.choices:
-            if data and set(data).issubset(set(self.choices.keys())):
-                self._valid = True
-        else:
-            self._valid = True
-        return self._valid
-
-    @property
-    def data(self):
-        if self._valid:
-            if self.choices and not self._raw_data:
-                return [self.choices[i] for i in self._data]
-            else:
-                return self._data
-        else:
-            raise ValueError('the data is not valid')
 
 
 class QuickSearch:
