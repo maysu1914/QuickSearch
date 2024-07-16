a2b59ccae47afa7ace6192183470631b09ec9c86
maysu1914 <mustafa.aysu@hotmail.com>
2024-07-16 23:30:17 +0300

    remove custom driver related codes and add some update

--- scraper/browsers.py
+++ scraper/browsers.py
@@ -1,20 +1,27 @@
-import urllib
+from urllib.parse import unquote
 
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium_stealth import stealth
 
 
 class CustomChrome(Chrome):
 
     def __init__(self, *args, extensions=None, **kwargs):
         kwargs['options'] = kwargs.get('options') or ChromeOptions()
-        extensions = extensions or ['driver/extensions/block_image_1_1_0_0.crx']
-        kwargs['executable_path'] = kwargs.get('executable_path') or 'driver/chromedriver.exe'
-        for extension in extensions:
+        for extension in extensions or []:
             kwargs['options'].add_extension(extension)
         super(CustomChrome, self).__init__(*args, **kwargs)
+        stealth(self,
+                languages=["en-US", "en"],
+                vendor="Google Inc.",
+                platform="Win32",
+                webgl_vendor="Intel Inc.",
+                renderer="Intel Iris OpenGL Engine",
+                fix_hairline=True,
+                )
         # self.set_window_position(-10000, 0)
 
     def open(self, url):
-        if urllib.parse.unquote(self.current_url) != url:
+        if unquote(self.current_url) != url:
             self.get(url)
